fde882cd73f13845c112bf21ce07dd74fa21d95b
mmsultanov-code <mmsultanov92@gmail.com>
2024-10-14 00:02:30 +0500

    formatted code

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
-import {EthereumRoutes} from "./src/routes/"
+import { EthereumRoutes } from './src/routes/'
 
-const bodyParser = require("body-parser")
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
-app.use("/wallet", EthereumRoutes)
+app.use(express.static('public'))
+app.use('/wallet', EthereumRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Your app is listening on port " + process.env.PORT)
+    console.log('Your app is listening on port ' + process.env.PORT)
 })
